980d780af0241f13e9961673ed09a3a42a258018
wangZuxi <WANGZUXI.23@ichat.sp.edu.sg>
2024-02-18 20:09:02 +0800

    God blast man

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,30 +1,42 @@
+
+
+// ID reference:
+// 1: Ads
+// 2: TOILETS
+// 3: BASINS
+// 4: TOILET ACCESSORIES
+// 5: MIRRORS/MIRROR CABINETS
+// 6: Toilets
+
+
+class Product {
+    constructor(model, genre, size, details, id) {
+      this.model = model;
+      this.genre = genre;
+      this.size = size;
+      this.details = details;
+      this.id = id;
+    }
+}
+
+const toilets = [
+    new Product(`Mayfair WC 8116`, `toilets`, ``, ``, `2-1`),
+    new Product(`Mayfair WC 8126`, `toilets`, ``, ``, `2-2`)
+]
+
+
 function changeIcon(){
     document.getElementById('menuIcon').innerHTML == 'menu' ? document.getElementById('menuIcon').innerHTML = 'close' : document.getElementById('menuIcon').innerHTML = 'menu'
 }
 
 
-function login(){
-    $.ajax({
-        url: "http://localhost:3000/api/users/login",
-        type: "POST",
-        // headers: {
-        //   "Content-Type": "application/json",
-        //   "Authorization": "Bearer your-access-token"
-        // },
-        data: { username: document.getElementById('username').value, password: document.getElementById('password').value },
-        dataType: "json",
-        success: function(responseData, textStatus, jqXHR) {
-            // Success callback function
-            // Executed when the request is successful
-            localStorage.setItem("token", responseData.results.token); // storing data :)
-            localStorage.setItem("id", responseData.results.userId);
-            window.location.href = "home.html"; // sends user to another page from js
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            // Error callback function
-            // Executed when an error occurs during the request
-            document.getElementById('errormsg').innerHTML = `Login Error, Status Code ${JSON.stringify(jqXHR.status)}: ${JSON.stringify(jqXHR.responseJSON.message)}`;
-        }
+let adsCount = 1;
+function switchup(){
+    adsCount += 1
+    adsCount == 6 ? adsCount = 1 : 1;
+    document.getElementById('ads').style.backgroundImage = `url(../images/1-${adsCount}.png)`
+}
 
-    });
+function viewMoar(){
+    window.location.href = './advertisements.html';
 }
